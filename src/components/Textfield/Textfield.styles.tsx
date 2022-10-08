9c67883fa2ee893664545a4993da0375a6525080
James Howell <me@jameshowell.dev>
2022-10-08 23:55:27 +0100

    feat(textfield): added textfield component

--- src/components/Textfield/Textfield.styles.tsx
+++ src/components/Textfield/Textfield.styles.tsx
@@ -1,3 +1,27 @@
 import styled, { css } from "styled-components";
 
-export const Textfield = styled("input")``;
+export const Textfield = styled("input")`
+  appearance: none;
+  background-color: ${(props) => props.theme.colors.textfields};
+  width: 100%;
+  box-sizing: border-box;
+  padding: 8px 12px;
+  color: ${(props) => props.theme.colors.headings};
+  font-family: ${(props) => props.theme.typography.fontFamily};
+  font-size: ${(props) => props.theme.typography.sizes.default};
+  line-height: ${(props) => props.theme.typography.lineHeight};
+  border: ${(props) => props.theme.general.borderWidth} solid
+    ${(props) => props.theme.colors.borders};
+
+  &:focus::not(&:focus-visible) {
+    outline: 0;
+    border-color: unset;
+  }
+
+  &:focus-visible {
+    border-color: ${(props) => props.theme.colors.interactive};
+    outline: ${(props) => props.theme.general.focusWidth} solid transparent;
+    outline-offset: ${(props) => props.theme.general.focusWidth};
+    box-shadow: ${(props) => props.theme.general.focusBoxShadow};
+  }
+`;
--- src/components/Textfield/Textfield.tsx
+++ src/components/Textfield/Textfield.tsx
@@ -1,3 +1,16 @@
+import { ComponentPropsWithoutRef, forwardRef } from "react";
 import * as S from "./Textfield.styles";
 
-export const Textfield = () => <div>here</div>;
+export interface TextfieldProps extends ComponentPropsWithoutRef<"input"> {}
+
+export const Textfield = forwardRef<HTMLInputElement, TextfieldProps>(
+  ({ "aria-invalid": ariaInvalid, required, type = "text", ...props }, ref) => (
+    <S.Textfield
+      aria-invalid={ariaInvalid}
+      aria-required={required}
+      ref={ref}
+      type={type}
+      {...props}
+    />
+  )
+);
