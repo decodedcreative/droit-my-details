7d4e3e9cca593ad4a93221600d0855e9178e3ed1
James Howell <me@jameshowell.dev>
2022-10-08 23:48:23 +0100

    feat(button): added button component

--- src/components/Button/Button.styles.tsx
+++ src/components/Button/Button.styles.tsx
@@ -1,3 +1,31 @@
 import styled, { css } from "styled-components";
+import { ButtonProps } from "./Button";
 
-export const Button = styled("button")``;
+export const Button = styled("button")<ButtonProps>`
+  background-color: ${(props) => props.theme.colors.background};
+  border: 1px solid ${(props) => props.theme.colors.interactive};
+  color: ${(props) => props.theme.colors.interactive};
+  cursor: pointer;
+  font-family: ${(props) => props.theme.typography.fontFamily};
+  font-size: ${(props) => props.theme.typography.fontSize};
+  word-break: break-word;
+  user-select: none;
+  border-radius: 4px;
+  padding: 10px 16px;
+
+  ${(props) => css`
+    &:focus-visible {
+      outline: 2px solid transparent;
+      outline-offset: 2px;
+      box-shadow: 0 0 0 2px ${props.theme.colors.background},
+        0 0 0 4px ${props.theme.colors.interactive};
+    }
+  `};
+
+  ${(props) =>
+    props.variant === "primary" &&
+    css`
+      background-color: ${props.theme.colors.interactive};
+      color: ${props.theme.colors.background};
+    `};
+`;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,16 @@
+import { ComponentPropsWithRef, forwardRef, ReactNode } from "react";
 import * as S from "./Button.styles";
+export interface ButtonProps extends ComponentPropsWithRef<"button"> {
+  children: ReactNode;
+  variant?: "primary" | "secondary";
+  type: "button" | "reset" | "submit";
+  as?: React.ElementType | keyof JSX.IntrinsicElements;
+}
 
-export const Button = () => <div>here</div>;
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, ...args }, ref) => (
+    <S.Button {...args} ref={ref}>
+      {children}
+    </S.Button>
+  )
+);
