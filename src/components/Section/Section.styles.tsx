889c5818151a86f6b7d08acaab1681aafe0b0191
James Howell <me@jameshowell.dev>
2022-10-09 20:52:25 +0100

    feat(section): added layout and spacing options

--- src/components/Section/Section.styles.tsx
+++ src/components/Section/Section.styles.tsx
@@ -1,12 +1,29 @@
 import styled, { css } from "styled-components";
-import { SectionProps, SectionItemProps } from "./Section";
+import {
+  SectionProps,
+  SectionItemProps,
+  SectionItemContentProps,
+} from "./Section";
 
-export const Section = styled("section")<SectionProps>``;
+export const Section = styled("section")<SectionProps>`
+  border: 0;
+  padding: 0;
+  margin: 0;
+`;
 
 export const SectionContent = styled("div")<SectionProps>`
-  @media screen and (max-width: 1280px) {
-    padding-left: ${(props) => props.theme.general.gridGutter};
-    padding-right: ${(props) => props.theme.general.gridGutter};
+  padding-bottom: 8px;
+
+  ${(props) =>
+    props.border === "bottom" &&
+    css`
+      border-bottom: ${(props) => props.theme.general.borderWidth} solid
+        ${(props) => props.theme.colors.borders};
+    `}
+
+  @media screen and (max-width: 960px) {
+    margin-left: ${(props) => props.theme.general.gridGutter};
+    margin-right: ${(props) => props.theme.general.gridGutter};
   }
 `;
 
@@ -18,11 +35,23 @@ export const SectionItem = styled("div")<SectionItemProps>`
       padding-bottom: ${(props) => props.theme.spacing.xl};
     `}
   ${(props) =>
+    props.topSpacing === "xl" &&
+    css`
+      padding-top: ${(props) => props.theme.spacing.xl};
+    `}
+  ${(props) =>
     props.verticalSpacing === "xxl" &&
     css`
       padding-top: ${(props) => props.theme.spacing.xxl};
       padding-bottom: ${(props) => props.theme.spacing.xxl};
     `}
+
+  ${(props) =>
+    props.bottomSpacing === "xl" &&
+    css`
+      padding-bottom: ${(props) => props.theme.spacing.xl};
+    `}
+
   ${(props) =>
     props.bottomSpacing === "xxl" &&
     css`
@@ -30,8 +59,20 @@ export const SectionItem = styled("div")<SectionItemProps>`
     `}
 `;
 
-export const SectionItemContent = styled("div")`
+export const SectionItemContent = styled("div")<SectionItemContentProps>`
   max-width: ${(props) => props.theme.general.gridMaxWidth};
   margin-left: auto;
   margin-right: auto;
+
+  ${(props) =>
+    props.columns &&
+    css`
+      @media screen and (min-width: 641px) {
+        display: grid;
+        column-gap: ${(props) => props.theme.general.gridGutter};
+        grid-template-columns: ${Array.from(Array(props.columns))
+          .map((item) => "1fr")
+          .join(" ")};
+      }
+    `}
 `;
--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -4,32 +4,42 @@ import * as S from "./Section.styles";
 export interface SectionProps {
   children: ReactNode;
   as?: React.ElementType | keyof JSX.IntrinsicElements;
+  border?: "top" | "bottom" | "vertical" | "horizontal" | boolean;
 }
 
 export interface SectionItemProps {
   children: ReactNode;
   bottomSpacing?: "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
+  topSpacing?: "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
+  columns?: number;
   verticalSpacing?: "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
 }
 
-export const Section = ({ children, ...args }: SectionProps) => (
+export interface SectionItemContentProps {
+  columns?: number;
+}
+
+export const Section = ({ children, border, ...args }: SectionProps) => (
   <S.Section {...args}>
-    <S.SectionContent>{children}</S.SectionContent>
+    <S.SectionContent border={border}>{children}</S.SectionContent>
   </S.Section>
 );
 
 const SectionItem = ({
   children,
   bottomSpacing,
+  topSpacing,
+  columns,
   verticalSpacing,
   ...args
 }: SectionItemProps) => (
   <S.SectionItem
     bottomSpacing={bottomSpacing}
+    topSpacing={topSpacing}
     verticalSpacing={verticalSpacing}
     {...args}
   >
-    <S.SectionItemContent>{children}</S.SectionItemContent>
+    <S.SectionItemContent columns={columns}>{children}</S.SectionItemContent>
   </S.SectionItem>
 );
 
