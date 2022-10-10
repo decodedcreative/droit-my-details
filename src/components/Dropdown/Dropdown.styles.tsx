247acc73fbb46de0a1b9d12e3d4ff356452c3047
James Howell <me@jameshowell.dev>
2022-10-10 22:44:08 +0100

    feat(dropdown): added style resets for react select

--- src/components/Dropdown/Dropdown.styles.tsx
+++ src/components/Dropdown/Dropdown.styles.tsx
@@ -1,7 +1,20 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import Select from "react-select";
 
 export const Dropdown = styled(Select)`
   width: 100%;
   border: 0;
+
+  .Select__control {
+    border-color: transparent;
+    background-color: ${(props) => props.theme.colors.textfields};
+  }
+
+  .Select__indicator-separator {
+    background-color: transparent;
+  }
+
+  .Select__value-container > * {
+    color: ${(props) => props.theme.colors.headings};
+  }
 `;
